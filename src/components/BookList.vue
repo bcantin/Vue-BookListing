f63f384c9082feb7e97cc58bff6f634b750c8070
Brad Cantin <bradc@knowbe4.com>
2020-03-19 14:37:19 -0500

    create booklist component

---
+++ src/components/BookList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+
+<script type="text/javascript">
+
+  export default {
+    name: 'BookList',
+    data() {
+      return {
+        title: 'All Books',
+      };
+    },
+  };
+
+</script>
+
+<style type="text/css">
+  h2, h2 {
+    font-weight: normal;
+  }
+</style>
