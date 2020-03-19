c6387153457203496b1aeb230105f0a55985e610
Brad Cantin <bradc@knowbe4.com>
2020-03-19 15:00:24 -0500

    Add BookList.vue

---
+++ src/components/BookItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>{{book.title}}:{{book.author}}</li>
+</template>
+
+<script type="text/javascript">
+
+  export default {
+    name: 'BookItem',
+    props: ['book']
+  }
+
+</script>
+
+<style type="text/css">
+
+  li {
+    display: block;
+    margin: 0 10px;
+  }
+
+</style>
